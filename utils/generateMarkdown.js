c181235b36460b1a8a42888db430edf464501b3d
Brianca Foster <missbee51@yahoo.com>
2020-09-12 21:29:02 -0400

    Updated-Portfolio-Page

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,23 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  * Description
+  This is a demostration on a brief about me and how this page is delevoped. The video below will show how the coding functions work and the result to the finishing page.
+  
+  * Table of Contents
+    -Breakdown of info need
+    -Answering the questions
+    -Showing the finished coding for the HTML
+ 
+    * Installation
+  - Inquirer
+  - Bookstrap
+ 
+  * Usage
+  
+  * License
+  - MIT
+`;
+}
+
+module.exports = generateMarkdown;
